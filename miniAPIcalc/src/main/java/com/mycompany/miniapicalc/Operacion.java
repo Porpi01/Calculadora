c18082c7646604efa983cdb3b2803f8e6372331f
Porpi01 <126757988+Porpi01@users.noreply.github.com>
2023-09-26 16:48:49 +0200

    Add files via upload

---
+++ miniAPIcalc/src/main/java/com/mycompany/miniapicalc/Operacion.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.miniapicalc;
+
+/**
+ *
+ * @author USER
+ */
+public class Operacion {
+    
+    private int numero1;
+    private int numero2;
+    private String operador;
+    double resultado;
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+   
+    
+}
