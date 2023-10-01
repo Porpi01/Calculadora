5c1997297f88003e7a83aadf923058547fe71c7b
Porpi01 <126757988+Porpi01@users.noreply.github.com>
2023-10-01 17:51:23 +0200

    Add files via upload

---
+++ miniAPIcalc3Monica/src/main/java/com/mycompany/miniapicalc2/ResponseBean.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.miniapicalc2;
+
+/**
+ *
+ * @author USER
+ */
+public class ResponseBean {
+    
+    private double operando1;
+    private double operando2;
+        private String operador;
+    double resultado;
+    private String errorDescription;
+
+    public double getOperando1() {
+        return operando1;
+    }
+
+    public void setOperando1(double operando1) {
+        this.operando1 = operando1;
+    }
+
+    public double getOperando2() {
+        return operando2;
+    }
+
+    public void setOperando2(double operando2) {
+        this.operando2 = operando2;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
+
+    public void setErrorDescription(String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
+
+    
+   
+}
